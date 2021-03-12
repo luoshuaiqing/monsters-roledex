cc953c6ededf2c1ea7c338ad82b71f5acdaf7bc9
Shuaiqing Luo <shuaiqin@usc.edu>
2021-03-12 00:22:00 -0800

    update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: '',
     }
   }
 
@@ -16,19 +18,16 @@ class App extends Component {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => this.setState({monsters: users}));
-    
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+
     return (
       <div className="App">
-        <CardList name="Shuaiqing">
-        {this.state.monsters.map((monster) => 
-            (
-              <h1 key={monster.id}>{monster.name}</h1>
-            )
-        )}        
-        </CardList>
+        <SearchBox placeholder='search monsters' handleChange={e => this.setState({searchField: e.target.value})} />
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );   
   }
