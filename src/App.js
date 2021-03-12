e37a96bf6f174a7761101fa636f39f683e40cdc8
Shuaiqing Luo <shuaiqin@usc.edu>
2021-03-11 17:16:22 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: [ {
-          name: 'Frankenstein'
-        }, {
-          name: 'Dracula'
-        }, {
-          name: 'Zombie'
-        }
-      ]
+      monsters: []
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
+    
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <p>{this.state.string}</p>
-          <p>{this.state.string}</p>
-          {/* <button onClick={() => this.setState({ string: 'Hello Shuaiqing' })}>Change Text</button> */}
-          <button onClick={() => this.state = { string: 'Hello Shuaiqing' }}>Change Text</button>
-          <button onClick={() => console.log(this.state) }>Print State</button>
-        </header>
+        <CardList name="Shuaiqing">
+        {this.state.monsters.map((monster) => 
+            (
+              <h1 key={monster.id}>{monster.name}</h1>
+            )
+        )}        
+        </CardList>
       </div>
     );   
   }
